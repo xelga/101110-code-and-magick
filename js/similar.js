98cd81a87ab44b3179cc6553168efc14cb219909
xelga <wanxelga@yandex.ru>
2017-12-16 16:31:30 +0300

    Добавлен модуль backend.js для работы с сервером;

--- js/similar.js
+++ js/similar.js
@@ -3,28 +3,36 @@
 (function () {
   var similarList = document.querySelector('.setup-similar-list');
   var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  var wizardQuantity = 4;
 
   var renderWizard = function (params) {
     var wizard = wizardTemplate.cloneNode(true);
 
     wizard.querySelector('.setup-similar-label').textContent = params.name;
-    wizard.querySelector('.wizard-coat').style.fill = params.coatColor;
-    wizard.querySelector('.wizard-eyes').style.fill = params.eyesColor;
+    wizard.querySelector('.wizard-coat').style.fill = params.colorCoat;
+    wizard.querySelector('.wizard-eyes').style.fill = params.colorEyes;
 
     return wizard;
   };
 
-  var createFragment = function () {
+  window.util.appendErrorContainer();
+
+  var onWizardsLoad = function (data) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < window.data.length; i++) {
-      fragment.appendChild(renderWizard(window.data[i]));
+    for (var i = 0; i < wizardQuantity; i++) {
+      fragment.appendChild(renderWizard(data[i]));
     }
 
-    return fragment;
+    similarList.appendChild(fragment);
   };
 
-  similarList.appendChild(createFragment());
+  var onWizardsError = function (errorMessage) {
+    var errorNode = window.util.createErrorNode(errorMessage);
+    window.util.removeErrorNode(errorNode);
+  };
+
+  window.backend.load(onWizardsLoad, onWizardsError);
 
   var setupSimilar = document.querySelector('.setup-similar');
   setupSimilar.classList.remove('hidden');
